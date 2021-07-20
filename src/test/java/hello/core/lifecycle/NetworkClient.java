f6aee3b8362638ed197c7cdc9218cc5f28e883f7
usang <usang6325@gmail.com>
2021-07-20 17:51:56 +0900

    빈 생명주기 애노테이션

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,14 +1,14 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
-
     }
 
     public void setUrl(String url) {
@@ -28,4 +28,18 @@ public class NetworkClient {
     public void disconnect(){
         System.out.println("close: " + url);
     }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @PreDestroy
+    public void close(){
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+
 }
