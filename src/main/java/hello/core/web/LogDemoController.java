70f3f8aa42ee5642e0713b696d72069e43146849
usang <usang6325@gmail.com>
2021-07-24 15:52:29 +0900

    스코프와 프록시

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,13 +14,15 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
-        MyLogger myLogger = myLoggerProvider.getObject();
         String requestUrl = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
+
         myLogger.setRequestUrl(requestUrl);
 
         myLogger.log("controller test");
